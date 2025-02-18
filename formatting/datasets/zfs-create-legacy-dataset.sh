b076868884e33e75eba3c197b37ea6d9c10b418e
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2025-02-17 19:01:30 -0500

    Wrote legacy dataset creation script.

---
+++ formatting/datasets/zfs-create-legacy-dataset.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+DATASET_NAME='legacy'
+function helptext {
+    echo "Usage: zfs-create-legacy-dataset.bash pool-name"
+    echo
+    echo 'Pass the name of the pool where you want to create this dataset.'
+    echo
+    echo 'This dataset is for storing data brought in from disparate other backup sources.'
+    echo 'Data in this dataset should eventually be organized into other datasets.'
+    echo
+    echo 'Warning: This script does not check validity — make sure your pool exists.'
+}
+
+## Validate parameters
+if [[ ! $# -eq 1 ]]; then
+    helptext >&2
+    exit 1
+fi
+
+## Create dataset
+set -e
+zfs create \
+    \
+    -O utf8only=no \
+    -O normalization=none \
+    \
+    -O canmount=on \
+    -O mountpoint=none \
+    \
+    "$1/$DATASET_NAME"
+exit $?
