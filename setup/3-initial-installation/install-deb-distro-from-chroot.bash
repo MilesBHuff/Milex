822693ef87222da2d3319ec711c832bb96626400
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2026-02-11 19:36:12 -0500

    Reworked the FSH code.

--- setup/3-initial-installation/install-deb-distro-from-chroot.bash
+++ setup/3-initial-installation/install-deb-distro-from-chroot.bash
@@ -896,41 +896,37 @@ fi; unset DO_IT
 echo ':: Modifying filesystem hierarchy...'
 
 ## `/var/www` needs to be moved to `/srv` so that it is treated the same as other web services.
-mkdir -p /var/www
-if [[ ! -L /var/www && ! -d /srv/www ]]; then
-    mv -f /var/www /srv/www
-    ln -sTv /srv/www /var/www
+if [[ ! -L '/var/www' ]]; then
+    [[ ! -d '/var/www' ]] && mkdir /var/www
+    mv -f   '/var/www' '/srv/www' #FIXME: Will fail if `/srv/www` already exists; we need logic that merges the two directories.
+    ln -sTv '/srv/www' '/var/www'
 fi
 
 ## This helps reflect dataset inheritance.
-if [[ ! -L /home/root ]]; then
-    ln -sTv /root /home/root
+if [[ ! -L '/home/root' ]]; then
+    ln -sTv '/root' '/home/root'
 fi
 
-# The following files need to be moved to a whitelisted directory and symlinked back.
-# Unfortunately, their associated applications recreate them, meaning that any symlinks would be deleted and replaced.
-#
-# if [[ ! -L /var/lib/apt/extended_states && ! -d /var/lib/apt/states ]]; then
-#     mkdir -p /var/lib/apt/states
-#     mv /var/lib/apt/extended_states /var/lib/apt/states/extended
-#     ln -sTv ./states/extended /var/lib/apt/extended_states
-# fi
-#
-# if [[ ! -L /var/lib/shells.state && ! -d /var/lib/shells ]]; then
-#     mkdir -p /var/lib/shells
-#     mv /var/lib/shells.state /var/lib/shells/state
-#     ln -sTv ./shells/state /var/lib/shells.state
-# fi
-
-## Ensure that certain key directories in `/var` remain tied to system snapshots.
+## Some items in `/var` need to be tied to system snapshots.
+## The criteria for inclusion is whether a rollback without the item would render the system's state inconsistent.
 VARKEEP_DIR='/varlib'
 mkdir -p "$VARKEEP_DIR"
 if [[ -d "$VARKEEP_DIR" ]]; then
-    declare -a VARKEEP_DIRS=('/var/lib/apt' '/var/lib/dkms' '/var/lib/dpkg' '/var/lib/emacsen-common' '/var/lib/sgml-base' '/var/lib/ucf' '/var/lib/xml-core') # '/var/lib/apt/states' '/var/lib/shells'
+    declare -a VARKEEP_DIRS=('lib/apt' 'lib/dkms' 'lib/dpkg' 'lib/emacsen-common' 'lib/sgml-base' 'lib/ucf' 'lib/xml-core') # 'lib/apt/states' 'lib/shells'
     for DIR in "${VARKEEP_DIRS[@]}"; do
-        if [[ -d "$DIR" && ! -L "$DIR" ]]; then
-            mv -f "$DIR" "$VARKEEP_DIR/"
-            ln -sTv "$VARKEEP_DIR/$(basename "$DIR")" "$DIR"
+        if [[ ! -L "/var/$DIR" ]]; then
+            [[ ! -d "/var/$DIR" ]] && mkdir "/var/$DIR"
+            mv -f "/var/$DIR" "$VARKEEP_DIR/"
+            ln -sTv "$VARKEEP_DIR/$DIR" "/var/$DIR"
+        fi
+    done
+    declare -a VARKEEP_FILES=() #WARN: The following files' associated applications recreate them, meaning that any symlinks are be deleted and replaced: 'lib/apt/extended_states' 'lib/shells.state'
+    for FILE in "${VARKEEP_FILES[@]}"; do
+        if [[ ! -L "/var/$FILE" ]]; then
+            [[ ! -f "/var/$FILE" ]] && continue
+            install -D "/var/$FILE" "$VARKEEP_DIR/$FILE"
+            rm -f "/var/$FILE"
+            ln -sTv "$VARKEEP_DIR/$FILE" "/var/$FILE"
         fi
     done
 fi
